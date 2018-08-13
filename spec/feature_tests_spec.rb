9ddc60ce2d2c4a939dfa3df984914a711a0401bb
John Forster <j_forster@hotmail.co.uk>
2018-08-13 12:36:17 +0100

    Add feature test for final product
    Adds a feature test that covers a full usage scenario: depositing,
    withdrawing and printing a statement.

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -12,4 +12,19 @@ describe 'Feature tests' do
     output.map! { |line| line.delete(' ') }
     expect(output).to include('|Date|Credit|Debit|Balance|')
   end
+
+  it 'is entirely functional' do
+    formatted_time = Time.new().strftime("%d/%m/%Y")
+    @bank_account.deposit(400)
+    @bank_account.withdraw(150)
+    @bank_account.deposit(33)
+    output = capture_stdout do
+      @bank_account.print
+    end
+    output.map! { |line| line.delete(' ') }
+    expect(output).to include('|Date|Credit|Debit|Balance|')
+    expect(output).to include("|#{formatted_time}|400.00||400.00|")
+    expect(output).to include("|#{formatted_time}||150.00|250.00|")
+    expect(output).to include("|#{formatted_time}|33.00||283.00|")
+  end
 end
