90b8a7729450a2f8d0f05b92e45557fc83a5d174
John Forster <j_forster@hotmail.co.uk>
2018-08-13 12:10:36 +0100

    Change helper_functions.rb to be clearer
    Extracted a `use_temporary_stdout_for` method for blocks of code that
    require a fresh stdout.

--- spec/helper_functions.rb
+++ spec/helper_functions.rb
@@ -1,9 +1,16 @@
 def capture_stdout
+  output = []
+  use_temporary_stdout_for do
+    yield
+    $stdout.rewind
+    output = $stdout.readlines
+  end
+  output.map(&:strip)
+end
+
+def use_temporary_stdout_for
   old_stdout = $stdout
   $stdout = StringIO.new
   yield
-  $stdout.rewind
-  output = $stdout.readlines
   $stdout = old_stdout
-  output.map(&:strip)
 end
